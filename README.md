c0dd9cf5a3802d15e572f6a2fc886b577f791f5f
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2022-01-24 22:48:02 +0800

    Multiple Changes
    1. Restored deleted files from commit aa173ef as it causes issues when trying to run the server
    2. Updated README
    3. Integrated Unicode.js and Checker.js files to web app
    4. Created a rudimentary GUI layout for basic testing, please change this however you like. The only goal of the commit is to prepare the backend of the web app.
    
    Note: The requirement (as how I understood it) states that UTF32 must be formatted as: xx xx xx where each hex is a nibble,but I forgot that when I was making the backend of the app. Thus prior to output,UTF-32 must be reformatted accordingly. Will try to clarify with Sir so that the formattingcould be done within Unicode.js itself.

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# ASCII Converter
+# Unicode Converter
 
-in partial fulfillment to CSARCH2, this repository was created to store... (to add send halp uwu).
+As a partial fulfillment to CSARCH2, this repository was created to store the required code and other related files to complete
+the requirement of building an application/web application that will serve as a culmination activity for the lessons taught in 
+the course.
+
+The final output of the group will depend on the assigned topic:
+1. Group 1 - Binary Converter
+2. Group 2 - IEEE-754 Decimal 32 translator
+3. Group 3 - BCD generator and translator
+4. Group 4 - IEEE-754 Binary 32 translator
+5. Group 5 - IEEE-754 Decimal 64 translator
+6. Group 6 - IEEE-754 Binary 64 translator
+7. Group 7 - Unicode
+
+# Instructions
+
+1. Input: Unicode (with invalid Unicode check)
+2. Output: UTF-8; UTF-16; UTF-32 (formatted as: xx xx xx; where x is hex nibble)
+3. Note: output with option to paste result in notepad
 
 ## Scripts
 
@@ -18,3 +35,12 @@ You may also see any lint errors in the console.
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
+
+## Group 7
+1. Alon-alon, Jason Miguel
+2. Cruz, Julianne Felice
+3. De Guzman, Cyril Ethan
+4. Escalona, Jose Miguel
+5. Rebong, Leana Hyacinth
+6. Roncal, Raphael
+7. Turk, Chadi
--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './unicode/Checker.js';
+import { Checker } from './unicode/Checker.js';
+import './unicode/Unicode.js';
+import { Unicode } from './unicode/Unicode.js';
 
 function App() {
-  return (
+  Test(); //Tests if Checker and Unicode are 'online'
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Title">
+          <h1>Unicode Converter</h1>
+          <hr style={{width:'100%'}}/>
+        </div>
+        <div className="Input">
+          <p>
+            Input Unicode:
+          </p>
+          <input className="Input-unicode" name="input"/>
+        </div>
+        <div className="Output">
+          <p>
+            Output:
+          </p>
+          <textarea readOnly className="Output-unicode" name="output" style={{marginBottom:'2%'}} />
+        </div>
+        <div className="Button-grid">
+          <button className="Button-convert" name='convert' onClick={Convert}>
+            Convert
+          </button>
+          <button className="Button-reset" name='reset' onClick={Reset}>
+            Reset
+          </button>
+        </div>
+        <div>
+          <hr style={{width:'100%'}}/>
+          <p>CSARCH2 S13 - Group 7 (2022)</p>
+        </div>
       </header>
     </div>
   );
+}
+
+/**
+ * Checks if Unicode and Checker is online
+ */
+function Test(){
+  let u = new Unicode();
+  let c = new Checker();
+  u.testUnicode();
+  c.testChecker();
+  u = null;
+  c = null;
+}
+
+function Convert(){
+  let u = new Unicode();
+  let c = new Checker();
+
+  var input = ""
+  /**
+   * TODO
+   * Extract text data from user-input to input variable 
+   */
+
+  if(c.CheckInput != null)
+    u.SetUnicode(input);
+
+  /**
+   * TODO
+   * Choose among two ways of getting resutling values:
+   * 1. As a list: u.GetAll(); contains UTF8,16,32, and char equivalent of the input Unicode
+   * 2. Individual: u.GetUTF8(), u.GetUTF16(), u.GetUTF32(), u.GetUTFChar()
+   */
+}
+
+function Reset(){
+  /**
+   * Delete contents of input and output
+   */
 }
 
 export default App;
